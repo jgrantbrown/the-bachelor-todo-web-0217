847196f1d5d576784a9703f70507090d7aaee358
jason brown <j.grant.brown@gmail.com>
2017-01-24 13:25:05 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,18 +1,18 @@
 def get_contestant_name(data,occupation)
           data.map{|season,datalist|
-          datalist.map{|list| return list[:name] if list[:"occupation"] == occupation}
+          datalist.map{|list| return list["name"] if list["occupation"] == occupation}
         }
 end
 
 def get_first_name_of_season_winner(data,season)
-    foundname=data[:"#{season}"][0][:"name"].split(" ")
+    foundname=data["season"][0]["name"].split(" ")
     foundname[0].to_s
 end
 
 def count_contestants_by_hometown(data,town)
   count = 0
       data.map{|season,datalist|
-      datalist.map{|list| count+= 1 if list[:"hometown"]== town}
+      datalist.map{|list| count+= 1 if list["hometown"]== town}
               }
       count
 end
