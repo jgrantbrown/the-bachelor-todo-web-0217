67b0dab72a275a96d52362dbabe5a8c8bbba59eb
jason brown <j.grant.brown@gmail.com>
2017-01-24 02:15:53 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,8 @@
-def get_first_name_of_season_winner(data, season)
-  # code here
-end
+def get_first_name_of_season_winner(data,season)
+  data[:"#{season}"][0][:"status"]=="Winner"
+    foundname=data[:"#{season}"][0][:"name"].split(" ")
+    foundname[0].to_s
+    end
 
 def get_contestant_name(data, occupation)
   # code here
