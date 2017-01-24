5e534bd2c55ed0908142d80cd8b46adfdce1b5b0
jason brown <j.grant.brown@gmail.com>
2017-01-24 15:42:22 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -21,3 +21,10 @@ def get_occupation(data,hometown)
     datalist.map{|list| return list["occupation"]  if list["hometown"]==hometown}
         }
  end
+
+ def get_average_age_for_season(data,season)
+   new_array=[]
+   data[:"#{season}"].map{|k| new_array<<k[:age].to_i}
+   avgage=new_array.inject{ |sum,x| sum + x }/new_array.size
+   return avgage.to_f.ceil
+ end
